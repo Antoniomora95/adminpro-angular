7d27080d680023e136764366e937c404f365e9b6
Antoniomora95 <morabautistantonio@gmail.com>
2019-02-10 10:37:26 -0600

    mMdificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// Rutas principales
-// Si nos encontramos algo que diga module
-// siempre va en la parte de los imports
+  /*Rutas principales
+ Si nos encontramos algo que diga module o rutas
+ siempre va en la parte de los imports*/
+// RUTAS
 import { APP_ROUTES } from './app.routes';
+
+// MODULOS
+import { PagesModule } from './pages/pages.module';
+
+// COMPONENTES
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
-  imports: [BrowserModule, APP_ROUTES],
+  imports: [
+    BrowserModule,
+    PagesModule,
+    APP_ROUTES
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
